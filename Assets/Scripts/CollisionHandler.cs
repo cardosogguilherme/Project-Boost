e82487c7c8ba3179c3b0eee45e36e74982657db1
Guilherme Cardoso <gcardoso@duck.com>
2023-05-27 18:12:34 +1200

    Load Next Level

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -14,11 +14,20 @@ public class CollisionHandler : MonoBehaviour
             case "Fuel":
                 break;
             case "Finish":
+                LoadNextLevel();
                 break;
             default:
                 ReloadLevel();
                 break;
         }
+    }
+
+    private void LoadNextLevel()
+    {
+        var nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        var nextIndex = nextSceneIndex == SceneManager.sceneCountInBuildSettings ? 0 : nextSceneIndex;
+        
+        SceneManager.LoadScene(nextIndex);
     }
 
     private void ReloadLevel()
